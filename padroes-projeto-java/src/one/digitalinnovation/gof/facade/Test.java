bb1d1261278c66e843399473172e4bb950760b2e
Leonardo Crestani <leogolincrestani@hotmail.com>
2023-04-27 20:01:55 -0300

    Add Singleton, Strategy, Facade

---
+++ padroes-projeto-java/src/one/digitalinnovation/gof/facade/Test.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.gof.facade;
+
+public class Test {
+    public static void main(String[] args) {
+        Facade facade = new Facade();
+        facade.migrarCliente("Leonardo", "14000000");
+    }
+}
